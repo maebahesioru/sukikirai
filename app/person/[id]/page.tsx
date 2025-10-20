7cb4d21ea85e036c75b526bb273a6bc5bc239581
maebahesioru <pikomaeba@gmail.com>
2025-10-20 23:35:18 +0900

    Remove generateStaticParams to fix 404 on dynamic routes

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,12 +1,6 @@
 import PersonPageClient from './PersonPageClient';
 
-export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
-
-export async function generateStaticParams() {
-  // 空の配列を返すことで、すべてのパラメータを動的に処理
-  return [];
-}
 
 interface PersonPageProps {
   params: {
